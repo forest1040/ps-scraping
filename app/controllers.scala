0525b1a3091351828ee86ca62603ffe8348fd9ba
Toshio Mori <forest1040@gmail.com>
2012-03-01 19:12:51 +0900

    HTMLスクレイピング処理を追加。

--- app/controllers.scala
+++ app/controllers.scala
@@ -2,6 +2,13 @@ package controllers
 
 import play._
 import play.mvc._
+import play.libs._
+import play.libs.WS._
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 
 object Application extends Controller {
     
@@ -9,6 +16,16 @@ object Application extends Controller {
     
     def index = {
         html.index("Your Scala application is ready!")
+    }
+    
+    def show = {
+    	val targetUrl = params.get("target_url")
+    	//Logger.info(targetUrl)
+		val res = WS.url(targetUrl).get()
+		val doc = Jsoup.parse(res.getString())
+    	val body = doc.select("body").first()
+    	//html.show("show", doc.text())
+    	html.show("show", body.html())
     }
     
 }
