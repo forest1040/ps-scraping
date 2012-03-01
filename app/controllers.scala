60a3d449346359d3adcbb6072812ba9ea6203c3a
Toshio Mori <forest1040@gmail.com>
2012-03-01 22:13:37 +0900

    HTMLをテキストに変換。

--- app/controllers.scala
+++ app/controllers.scala
@@ -6,9 +6,8 @@ import play.libs._
 import play.libs.WS._
 
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
-import org.jsoup.select.Elements
+import org.jsoup.nodes._
+import org.jsoup.select._
 
 object Application extends Controller {
     
@@ -18,14 +17,38 @@ object Application extends Controller {
         html.index("Your Scala application is ready!")
     }
     
-    def show = {
+    def showByHtml = {
     	val targetUrl = params.get("target_url")
-    	//Logger.info(targetUrl)
 		val res = WS.url(targetUrl).get()
 		val doc = Jsoup.parse(res.getString())
     	val body = doc.select("body").first()
     	//html.show("show", doc.text())
     	html.show("show", body.html())
+    }
+    
+    def showByText = {
+    	val targetUrl = params.get("target_url")
+		val res = WS.url(targetUrl).get()
+		val doc = Jsoup.parse(res.getString())
+    	//val body = doc.select("body").first()
+
+		//val strings = new java.util.ArrayList[String]
+		var str = ""
+		val nd = new NodeTraversor(new NodeVisitor() {
+			override def tail(node : Node, depth : Int) = {
+				if (node.isInstanceOf[TextNode]) {
+					val textNode = node.asInstanceOf[TextNode]
+					val text = textNode.getWholeText()
+					//Logger.info(text)
+					str += text + "\n"
+				}
+			}
+			override def head(node : Node, depth : Int) = {
+			}
+		})
+		nd.traverse(doc.body());
+    	
+    	html.show("show", "<pre>" + str + "</pre>")
     }
     
 }
